8098117f53b591e7b7fd0db426e22cbc9a775b84
Matěj Trakal <mtrakal@gmail.com>
2017-08-15 11:10:37 +0200

    Main branch

---
+++ app/src/main/java/cz/mtrakal/branchissue/Asdfff.kt
@@ -0,0 +1,10 @@
+package cz.mtrakal.branchissue
+
+/**
+ * Created by mtrakal on 15.08.2017.
+ */
+class Asdfff {
+    val i = 0
+
+    constructor()
+}
---
+++ app/src/main/java/cz/mtrakal/branchissue/Asfd.java
@@ -0,0 +1,17 @@
+package cz.mtrakal.branchissue;
+
+/**
+ * Created by mtrakal on 15.08.2017.
+ */
+
+public class Asfd {
+    public int i = 0;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
